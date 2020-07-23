a8471f622d64f664dc63a359133f575606c67d9b
Andrew Chen <andrewchen@plaid.com>
2020-07-23 11:16:22 -0700

    remove trailing backslash from links

--- LinkDemo-ObjC/LinkDemo-ObjC/ViewController+LinkToken.m
+++ LinkDemo-ObjC/LinkDemo-ObjC/ViewController+LinkToken.m
@@ -13,13 +13,13 @@
 @implementation ViewController (LinkToken)
 
 // MARK: Start Plaid Link with custom configuration using a Link token
-// For details please see https://plaid.com/docs/#create-link-token/
+// For details please see https://plaid.com/docs/#create-link-token
 - (void)presentPlaidLinkUsingLinkToken {
 
     #warning Replace <#GENERATED_LINK_TOKEN#> below with your link_token
     // In your production application replace the hardcoded linkToken below with code that fetches an link_token
     // from your backend server which in turn retrieves it securely from Plaid, for details please refer to
-    // https://plaid.com/docs/#create-link-token/
+    // https://plaid.com/docs/#create-link-token
     NSString* linkToken = @"<#GENERATED_LINK_TOKEN#>";
 
     // <!-- SMARTDOWN_PRESENT_LINKTOKEN -->
--- LinkDemo-Swift/LinkDemo-Swift/ViewController+LinkToken.swift
+++ LinkDemo-Swift/LinkDemo-Swift/ViewController+LinkToken.swift
@@ -10,13 +10,13 @@ import LinkKit
 extension ViewController {
 
     // MARK: Start Plaid Link with custom configuration using a Link token
-    // For details please see https://plaid.com/docs/#create-link-token/
+    // For details please see https://plaid.com/docs/#create-link-token
     func presentPlaidLinkUsingLinkToken() {
 
         #warning("Replace <#GENERATED_LINK_TOKEN#> below with your link_token")
         // In your production application replace the hardcoded linkToken below with code that fetches an link_token
         // from your backend server which in turn retrieves it securely from Plaid, for details please refer to
-        // https://plaid.com/docs/#create-link-token/
+        // https://plaid.com/docs/#create-link-token
         let linkToken = "<#GENERATED_LINK_TOKEN#>"
 
         // <!-- SMARTDOWN_PRESENT_LINKTOKEN -->
