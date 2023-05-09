a55b483b2b31fdac91b99b48956469fdd238fe36
dtroupe <dtroupe@plaid.com>
2023-05-09 14:42:08 -0700

    configuration comments

--- LinkDemo-ObjC/LinkDemo-ObjC/ViewController+LinkToken.m
+++ LinkDemo-ObjC/LinkDemo-ObjC/ViewController+LinkToken.m
@@ -25,14 +25,31 @@
 
     PLKLinkTokenConfiguration* linkConfiguration = [PLKLinkTokenConfiguration createWithToken:@"<#GENERATED_LINK_TOKEN#>"
                                                                                     onSuccess:^(PLKLinkSuccess *success) {
+
+        // Closure is called when a user successfully links an Item. It should take a single LinkSuccess argument,
+        // containing the publicToken String and a metadata of type SuccessMetadata.
+        // Ref - https://plaid.com/docs/link/ios/#onsuccess
         NSLog(@"public-token: %@ metadata: %@", success.publicToken, success.metadata);
     }];
+
+    // Optional closure is called when a user exits Link without successfully linking an Item,
+    // or when an error occurs during Link initialization. It should take a single LinkExit argument,
+    // containing an optional error and a metadata of type ExitMetadata.
+    // Ref - https://plaid.com/docs/link/ios/#onexit
     linkConfiguration.onExit = ^(PLKLinkExit * exit) {
         if (exit.error) {
             NSLog(@"exit with %@\n%@", exit.error, exit.metadata);
         } else {
             NSLog(@"exit with %@", exit.metadata);
         }
+    };
+
+    // Optional closure is called when certain events in the Plaid Link flow have occurred, for example,
+    // when the user selected an institution. This enables your application to gain further insight into
+    // what is going on as the user goes through the Plaid Link flow.
+    // Ref - https://plaid.com/docs/link/ios/#onevent
+    linkConfiguration.onEvent = ^(PLKLinkEvent * event) {
+        NSLog(@"Link event %@", event);
     };
 
     NSError *createError = nil;
@@ -44,7 +61,6 @@
     } else if (createError) {
         NSLog(@"Unable to create PLKHandler due to: %@", createError);
     }
-
 }
 
 @end
