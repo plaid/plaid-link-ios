7afb1afa64552149fd4091eb569dca79e318dab0
dtroupe <dtroupe@plaid.com>
2023-05-10 08:13:17 -0700

    simplify objective-c example

--- LinkDemo-ObjC/LinkDemo-ObjC/ViewController.h
+++ LinkDemo-ObjC/LinkDemo-ObjC/ViewController.h
@@ -8,20 +8,11 @@
 #import <UIKit/UIKit.h>
 #import <LinkKit/LinkKit.h>
 
-@protocol LinkOAuthHandling
-@property (readonly) id<PLKHandler> linkHandler;
-@end
-
-@interface ViewController : UIViewController <LinkOAuthHandling>
+@interface ViewController : UIViewController
 
 @property (readwrite) id<PLKHandler> linkHandler;
 
-#pragma mark - PublicKey Configuration (Deprecated)
-// Integrating LinkKit using public key has been deprecated in favor of Link Tokens
-// hence the public key related oauthNonce and oauthRedirect properties are deprecated as well.
-// For more information on how to migrate to Link Tokens see https://plaid.com/docs/link/link-token-migration-guide/
-@property (readonly) NSString* oauthNonce;
-@property (readonly) NSURL* oauthRedirectUri;
+- (void)presentPlaidLinkUsingLinkToken;
 
 @end
 
