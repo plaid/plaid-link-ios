a979233c9c4a59be096d7359cae10b5d32fb5a72
dtroupe <dtroupe@plaid.com>
2024-06-20 12:23:38 -0700

    add on dismiss

--- LinkDemo-SwiftUI/LinkDemo-SwiftUI/ContentView.swift
+++ LinkDemo-SwiftUI/LinkDemo-SwiftUI/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
         }
         .fullScreenCover(
             isPresented: $isPresentingLink,
+            onDismiss: { isPresentingLink = false },
             content: {
                 if let linkController {
                     linkController
